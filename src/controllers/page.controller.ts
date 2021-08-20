40a987dba7c505bbe97a5b3602c85d14ee077de0
Julio Cesar <juliocsrmf@gmail.com>
2021-08-20 10:32:56 -0300

    Criando template e separando as views

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-
+    res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    
 }
 
 export const cats = (req: Request, res: Response) => {
