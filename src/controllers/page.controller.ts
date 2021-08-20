aba0c62f935a3e2bea14d5e49f9782eb851b2700
Julio Cesar <juliocsrmf@gmail.com>
2021-08-20 10:48:53 -0300

    Modificando dados em cada página

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,17 +1,34 @@
 import { Request, Response } from 'express';
+import { Banner } from '../types/banner';
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page');
+    let banner: Banner = {
+        title: 'Todos os animais',
+        background: 'allanimals.jpg'
+    }
+    res.render('pages/page', { banner });
 }
 
 export const dogs = (req: Request, res: Response) => {
-    
+    let banner: Banner = {
+        title: 'Cachorros',
+        background: 'banner_dog.jpg'
+    }
+    res.render('pages/page', { banner });
 }
 
 export const cats = (req: Request, res: Response) => {
-
+    let banner: Banner = {
+        title: 'Gatos',
+        background: 'banner_cat.jpg'
+    }
+    res.render('pages/page', { banner });
 }
 
 export const fishes = (req: Request, res: Response) => {
-
+    let banner: Banner = {
+        title: 'Peixes',
+        background: 'banner_fish.jpg'
+    }
+    res.render('pages/page', { banner });
 }
---
+++ src/types/banner.ts
@@ -0,0 +1,4 @@
+export type Banner = {
+    title: string,
+    background: string
+}
